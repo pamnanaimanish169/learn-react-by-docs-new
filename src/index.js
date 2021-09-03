9991b9515c29d54ef96a73a278b4d7447910d697
pamnanaimanish169 <manishpamnani169@gmail.com>
2021-09-03 11:04:33 +0530

    Rendering Elements

--- src/index.js
+++ src/index.js
@@ -4,26 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user) {
-  return  user.firstName  + ' ' + user.lastName;
-}
-
-const user  = {
-  firstName   : 'Harper',
-  lastName    : 'Perez'
-};
+const currentTime = new Date();
+const futureDate  = new Date(2021,8,3,17);
+const timeRemaining = Math.abs((currentTime - futureDate)).toString();
 
 const element = (
 
   <div>
-    <label>Name:</label>
-    <input type="text" placeholder="Enter Name" id="name" name="name"/>
-    <br />
-    <label>Age:</label>
-    <input type="number" placeholder="Enter Age" id="age" name="age"/>
-    <br />
-    <label>Gender:</label>
-    <input  type="text" placeholder="Enter gender" id="gender" name="gender" />
+    <h1>Current time is {currentTime.toString()}</h1>
+
+    <h1>Future date is {futureDate.toString()}</h1>
+
+    <h1>Difference is {( (currentTime - futureDate).toString() )} in milliseconds</h1>
+
+    <h1>{Math.ceil(timeRemaining  / 1000) } in seconds</h1>
+
+    <h1>{ Math.ceil(timeRemaining  / (1000 * 60))} in minutes</h1>
+
+    <h1>{ Math.ceil(timeRemaining  / (1000 * 60 * 60) )} in hours</h1>
+    
   </div>
 );
 
