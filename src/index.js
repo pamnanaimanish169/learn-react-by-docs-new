21833bc2cf3e8d74ffd94b7e2be62f608ce360db
pamnanaimanish169 <manishpamnani169@gmail.com>
2021-09-08 09:26:18 +0530

    Handling Events

--- src/index.js
+++ src/index.js
@@ -4,90 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// function Clock(props) {
-//   return(
-//     <div>
-//       <h1>Hello, World!</h1>
-//       <h2>It is {props.date.toLocaleTimeString()}</h2>
-//     </div>
-//   )
-// }
 
-// Convert function(Clock()) to class(class Clock)
-// class Clock extends React.Component {
-  
-//   // Add a class **constructor** that assigns the initial this.state.
-//   constructor(props)  {
-//     super(props);
-//     this.state  = {date : new Date()};
-//   }
-
-//   componentDidMount() {
-//     this.timerID  = setInterval(
-//       ()  =>  this.tick(),
-//       1000
-//     );
-//   }
-
-//   componentWillUnmount()  {
-//     clearInterval(this.timerID);
-//   }
-
-//   tick()  {
-//     this.setState({
-//       date  : new Date()
-//     });
-//   }
-
-//   render() {
-//     return(
-//       <div>
-//         <h1>Hello, World!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-//       </div>
-//     ) 
-//   };
-// }
-
-class Name extends React.Component {
-
-  // constructor
-  constructor(props) {
+class Toggle extends React.Component  {
+  constructor(props)  {
     super(props);
 
-    // Initial state.
-    this.state  = { name  : "Manish", address : "Malviya Nagar", city : "Jaipur", state : "Rajasthan"  };
+    this.state  = {isToggleOn : true};
+
+    // Syntax to defien the function
+    this.handleClick  = this.handleClick.bind(this);
   }
 
-  // componentWillMount
-  componentWillMount()  {
-    setTimeout(() =>  {
-      this.setState({ name  : "Pralave",  address : "Jagatpura", city : "Jaipur", state : "Rajasthan" });
-    },500);
-  }
-
-  // componentWillUnMount
-  componentWillUnmount()  {
-    // This is to save the memeory leaks so that this variable is destroyed when the we move on to 
-    // another component.
-    this.setState({ name  : "" });
+  handleClick() {
+    // Update the value/state of the isToggleOn
+    // this.setState(prevState =>  ({
+    //   isToggleOn : !prevState.isToggleOn
+    // }));
+    console.log('this is', this);
   }
 
   render()  {
-    return(
-      <div>
-        <h1>Hello, World!</h1>
-        <h2>It is {this.state.name}</h2>
-        <p> <b>Address</b>  {this.state.address}</p>
-        <p> <b>City</b> {this.state.city}</p>
-        <p> <b>State</b> {this.state.state}</p>
-      </div>
-    );
+    return  (
+      // <button onClick={this.handleClick}>
+      //   {this.state.isToggleOn  ? 'ON'  : 'OFF'}
+      // </button>
+      <button onClick={() =>  this.handleClick()}>
+         {this.state.isToggleOn  ? 'ON'  : 'OFF'}
+      </button>
+    )
   }
 }
 
 ReactDOM.render(
-  <Name  />,
+  <Toggle  />,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
