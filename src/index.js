96e263232e6402468e8eb85c6c65b51d4df91152
pamnanaimanish169 <manishpamnani169@gmail.com>
2021-09-15 09:19:11 +0530

    Forms

--- src/index.js
+++ src/index.js
@@ -4,27 +4,59 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function ListItem(props)  {
-  return  <li>{props.value}</li>;
+class Reservation extends React.Component {
+  constructor(props)  {
+    super(props);
+
+    this.state  = { 
+      isGoing : true,
+      numberOfGuests  : 2
+    };
+
+    this.handleInputChange  = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event)  {
+    const target  = event.target;
+    const value   = target.type ==  'checkbox'  ? target.checked  : target.value;
+    const name    = target.name;
+
+    console.log(name);
+    console.log(value);
+
+    this.setState({
+      [name]  : value
+    });
+  }
+
+  render()  {
+    return(
+      <form>
+        <label>
+          Is going:
+          <input
+          name="isGoing"
+          type="checkbox"
+          checked={this.state.isGoing}
+          onChange={this.handleInputChange} />
+        </label>
+        <br />
+
+        <label>
+          Number of guests:
+          <input
+          name="numberOfGuests"
+          type="number"
+          value={this.state.numberOfGuests}
+          onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
 }
-
-function  NumberList(props) {
-  const numbers   = props.numbers;
-  const listItems = numbers.map((number)  =>  
-    <ListItem key={number.toString()} value={number}  />
-  );
-
-  return  (
-    <ul>
-      {listItems}
-    </ul>
-  );
-}
-
-const numbers = [1, 2,  3,  4,  5];
 
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <Reservation  />,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
